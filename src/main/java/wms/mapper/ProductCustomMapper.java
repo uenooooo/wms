016bad0cff14ad76c0380ade183bcdfaf6210e25
uenooooo <lain09.17@gmail.com>
2025-12-28 02:22:18 +0900

    del not required

--- src/main/java/wms/mapper/ProductCustomMapper.java
+++
@@ -1,9 +0,0 @@
-package wms.mapper;
-
-import org.apache.ibatis.annotations.Param;
-
-import wms.model.Product;
-
-public interface ProductCustomMapper {
-	Product selectByProductCd(@Param("productCd") String productCd);
-}
