534cda6d2b9e115578fd5955786a550e421796c7
uenooooo <151648192+uenooooo@users.noreply.github.com>
2025-12-28 20:54:10 +0900

    mod made stock insert in insert product

--- src/main/java/wms/service/ProductService.java
+++ src/main/java/wms/service/ProductService.java
@@ -18,16 +18,26 @@ public class ProductService {
 	}
 
 	public void addProduct(String productCd, String productName, int price) {
-		insertProduct(productCd, productName, price);
-
+		Long insProductId = insertProduct(productCd, productName, price);
+		insertStock(insProductId);
 	}
 
-	private void insertProduct(String productCd, String productName, int price) {
+	private Long insertProduct(String productCd, String productName, int price) {
 		Product productData = new Product();
 		productData.setProductCd(productCd);
 		productData.setProductName(productName);
 		productData.setPrice(price);
 		productData.setCrePrg(this.getClass().getName());
 		productMapper.insertSelective(productData);
+		Long insProductId;
+		return insProductId;
+	}
+
+	private void insertStock(Long productId) {
+		Stock stockData = new Stock();
+		stockData.setProductId(productId);
+		stockData.setQuantity(0);
+		stockData.setCrePrg(this.getClass().getName());
+		stockMapper.insertSelective(stockData);
 	}
 }
